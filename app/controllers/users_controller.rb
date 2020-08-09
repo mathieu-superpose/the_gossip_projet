78dc40da2b9005d65290f2c6d4814a7d464a2ecb
Mathieu <mathieu.joly@lrguas.ch>
2020-08-09 21:17:07 +0200

    up new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+  
 
   def show
     @user = current_user
@@ -7,6 +8,7 @@ class UsersController < ApplicationController
   
   def new
     @user = User.new
+    @cities = City.all
   end 
 
 
